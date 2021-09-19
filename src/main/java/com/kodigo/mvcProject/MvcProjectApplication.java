56c0b98e5f05b1dbe8f04f2276ecb1054ba63b9b
CarlosP10 <00028516@uca.edu.sv>
2021-09-19 12:13:33 -0600

    [CREATE] Project

---
+++ src/main/java/com/kodigo/mvcProject/MvcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kodigo.mvcProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcProjectApplication.class, args);
+	}
+
+}
